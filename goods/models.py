adb61c3b6ba69f4e9ea85bce605e9153d82e3005
egortens1a <120414001+egortens1a@users.noreply.github.com>
2024-08-01 13:27:05 +0300

    Added Fixtures

--- goods/models.py
+++ goods/models.py
@@ -4,13 +4,15 @@ from django.db import models
 
 class Categories(models.Model):
     name = models.CharField(max_length=150, unique=True, verbose_name='Название')
-    slug = models.SlugField(max_length=150, unique=True, null=True, verbose_name='URL')
+    slug = models.SlugField(max_length=150, unique=True, blank=True, verbose_name='URL')
 
     class Meta:
         db_table = 'category'
         verbose_name = 'Категорию'
         verbose_name_plural = 'Категории'
 
+    def __str__(self) -> str:
+        return self.name
 
 class Products(models.Model):
     name = models.CharField(max_length=150, unique=True, verbose_name='Название')
@@ -26,3 +28,6 @@ class Products(models.Model):
         db_table = 'product'
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
+
+    def __str__(self) -> str:
+        return f'{self.name}, Количество:{self.quantity}'
--- main/views.py
+++ main/views.py
@@ -1,11 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from goods.models import Categories
+
 # Create your views here.
 def index(request):
+
+    categories = Categories.objects.all()
     context = {
         'title': 'Home',
-        'content': 'Главная страница - HOME'
+        'content': 'Главная страница - HOME',
+        'categories': categories
     }
 
     return render(request, 'main\index.html', context)
