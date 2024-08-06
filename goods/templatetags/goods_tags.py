ed03338e8ca2e1e448891de6b977f617165a0db0
egortens1a <120414001+egortens1a@users.noreply.github.com>
2024-08-06 23:52:20 +0300

    added filter in products, urlencode

--- goods/templatetags/goods_tags.py
+++ goods/templatetags/goods_tags.py
@@ -1,3 +1,4 @@
+from django.utils.http import urlencode
 from django import template
 
 from goods.models import Categories
@@ -8,3 +9,9 @@ register = template.Library()
 @register.simple_tag()
 def tag_categories():
     return Categories.objects.all()
+
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwargs):
+    que = context['request'].GET.dict()
+    que.update(kwargs)
+    return urlencode(que)
--- goods/views.py
+++ goods/views.py
@@ -1,16 +1,34 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import get_list_or_404, render
 
 import goods
 from goods.models import Categories, Products
 
 # Create your views here.
-def catalog(request):
+def catalog(request, category_slug):
 
-    goods = Products.objects.all()
+    order_by = request.GET.get('order_by', None)
+    on_sale = request.GET.get('on_sale', None)
+    page = request.GET.get('page', 1)
+
+    if category_slug=='all':
+        goods = Products.objects.all()
+    else:
+        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+    if order_by and order_by != 'default':
+        goods = goods.order_by(order_by)
+
+
+    paginator = Paginator(goods, 3)
+    current_page = paginator.page(int(page))
 
     context = {
         'title':'Home - Каталог',
-        'goods': goods,
+        'goods': current_page,
+        'slug_url': category_slug
     }
     return render(request, 'goods\catalog.html', context)
 
