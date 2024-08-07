452410fb742d83445f77df1b32d9bf65beb6984f
egortens1a <120414001+egortens1a@users.noreply.github.com>
2024-08-08 00:29:35 +0300

    full text search GOTOV, added psql

--- goods/views.py
+++ goods/views.py
@@ -3,16 +3,20 @@ from django.shortcuts import get_list_or_404, render
 
 import goods
 from goods.models import Categories, Products
+from goods.utils import q_search
 
 # Create your views here.
-def catalog(request, category_slug):
+def catalog(request, category_slug=None):
 
+    page = request.GET.get('page', 1)
     order_by = request.GET.get('order_by', None)
     on_sale = request.GET.get('on_sale', None)
-    page = request.GET.get('page', 1)
+    query = request.GET.get('q', None)
 
     if category_slug=='all':
         goods = Products.objects.all()
+    elif query:
+        goods = q_search(query)
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
@@ -20,8 +24,7 @@ def catalog(request, category_slug):
         goods = goods.filter(discount__gt=0)
     if order_by and order_by != 'default':
         goods = goods.order_by(order_by)
-
-
+        
     paginator = Paginator(goods, 3)
     current_page = paginator.page(int(page))
 
